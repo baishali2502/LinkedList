4e91e6304007b8381e51be1a63514a7a42b2d07b
baishali2502 <baishali2502@gmail.com>
2023-12-14 20:09:44 +0530

    Added UC-1:Insert node at first

--- LinkedList/src/com/bridgelabz/linkedlist/LinkedList.java
+++ LinkedList/src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,5 +1,29 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList {
 
+public class LinkedList<T>
+{
+	Node<T> head = null;
+	Node<T> tail = head;
+    int size;
+//---------------------------------- UC-1 ---------------------------------------
+    /*
+	 * @desc:This method inserts new element at the beginning of the list
+	 * 
+	 * @params:element to be added
+	 * 
+	 * @returns:none
+	 */
+	void insertFirst(T val) {
+		Node<T> newnode = new Node<>(val);
+		if(head==null) {
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			newnode.next = head;
+			head = newnode;
+		}
+	}
 }
--- LinkedList/src/com/bridgelabz/linkedlist/Node.java
+++ LinkedList/src/com/bridgelabz/linkedlist/Node.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.linkedlist;
 
-public class Node {
-
+public class Node<T> 
+{
+	T data;
+	Node<T> next;
+	Node(T data,Node<T> next){
+		this.data = data;
+		this.next = next;
+	}
+	Node(T data){
+		this.data = data;
+		next = null;
+	}
 }
