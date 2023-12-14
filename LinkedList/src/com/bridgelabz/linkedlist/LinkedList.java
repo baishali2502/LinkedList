ad5cd9fbf083e7e06f83b9849c2d3f0825496feb
baishali2502 <baishali2502@gmail.com>
2023-12-14 20:51:36 +0530

    Added main method

--- LinkedList/src/com/bridgelabz/linkedlist/LinkedList.java
+++ LinkedList/src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList {
 
+public class LinkedList<T>
+{
+	Node<T> head=null; // represents head of linked-list
+    Node<T> tail = head;//represent tail of linked-list
+    int size;// size of linked-list
 }
--- LinkedList/src/com/bridgelabz/linkedlist/Main.java
+++ LinkedList/src/com/bridgelabz/linkedlist/Main.java
@@ -1,9 +1,86 @@
 package com.bridgelabz.linkedlist;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		// UC-1:Creating new linked-list
+		LinkedList<Integer> l = new LinkedList<Integer>();
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Operations:\n1.Insert at first\n"
+				+ "2.Insert at last\n"
+				+ "3.Insert at a certain position\n"
+				+ "4.Size of linked-list\n"
+				+ "5.Delete at first\n"
+				+ "6.Delete at last\n"
+				+ "7.Delete at any position\n"
+				+ "8.search\n"
+				+ "9.sort\n"
+				+ "Choose: ");
+		int choice = s.nextInt();
+		while(choice!=-1) 
+		{
+		  if(choice==1)
+		  {
+			System.out.print("Enter number :");
+			int val = s.nextInt();
+			l.insertFirst(val);
+			System.out.println("Node added at first position\n");
+		  }
+		  else if(choice==2)
+		  {
+			System.out.print("Enter number :");
+			int val = s.nextInt();
+			l.insertLast(val);
+			System.out.println("Node added at the end\n");
+		  }
+		  else if(choice==3)
+		  {
+			  System.out.print("Enter number :");
+			  int val = s.nextInt();
+			  System.out.print("Enter position :");
+			  int pos = s.nextInt();
+			  l.insertAtP(val,pos-1);
+			  System.out.println("Node added at position = "+pos+"\n");
+		  }
+		  else if(choice==4)
+		  {
+			  int n = l.size();
+			  System.out.println("Size of linked-list = "+n+"\n");
+		  }
+		  else if(choice==5)
+		  {
+			  l.deleteFirst();
+		  }
+		  else if(choice==6)
+		  {
+			  l.deleteLast();
+			  
+		  }
+		  else if(choice==7)
+		  {
+			  System.out.print("Enter number :");
+			  int val = s.nextInt();
+			  l.deleteByElem(val);
+		  }
+		  else if(choice==8)
+		  {
+			  System.out.print("Enter number :");
+			  int val = s.nextInt();
+			  int pos = l.search(val);
+		  }
+		  else if(choice==9)
+		  {
+			  l.sort();
+		  }
+		  
+		  System.out.print("Enter choice :");
+		  choice = s.nextInt();
+		}
+		
 
 	}
 
